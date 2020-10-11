e714e7feded02a09ff49ea9b3281014fe2f00384
Komii1412 <djdmuxd@gmail.com>
2020-10-11 23:09:21 +0800

    console input/output

---
+++ src/com/komii/cyoafi/Main.kt
@@ -0,0 +1,11 @@
+package com.komii.cyoafi
+
+import java.util.*
+
+fun main() {
+    val inputReader = Scanner(System.`in`)
+    var name:String = inputReader.nextLine()
+    println("Hello, $name")
+    name = "String"
+    println("Hello, $name")
+}
